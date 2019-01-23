800f58f6f02515ffc495266506cbeb3c6347d80c
Winter Wilson <ww633114@ohio.edu>
2019-01-22 19:20:24 -0500

    Commit git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Winter Wilson
+ *        Date: 01.22.2019
+ * Description: Code for Lab 2, CS 2400
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *argv[]) {
+
+     cout << "Hello Git!!" << endl;
+     return 0;
+ }//main
